221b0dad313b82e0dc91c8d7b1fa483daafcc6d8
Steve <steven.helferich@gmail.com>
2015-09-25 14:48:30 -0700

    added remaining issue and issue-comment manipulation methods

--- linker.py
+++ linker.py
@@ -17,6 +17,71 @@ class Git(object):
 		issues = request.json()
 		return issues
 
+	def getIssue(self, repo, issue):
+		url = self.base_url+"repos/"+self._username+"/"+repo+"/issues/"+issue
+		request = requests.get(url, auth=(self._username, self._password))
+		issue = request.json()
+		return issue
+
+	def raiseIssue(self, repo, title, body, aasignee=None, milestone=None, 
+		labels=None):
+		url = self.base_url+"repos/"+self._username+"/"+repo+"/issues"
+		payload = {}
+		payload["title"] = title
+		payload["body"] = body
+		if assignee: payload["assignee"] = assignee
+		if milestone: payload["milestone"] = milestone
+		if labels:
+			payload["labels"] = []
+			for label in labels:
+				payload["labels"].append(label)
+		request = requests.post(url, auth=(self._username, self._password),
+			data=payload)
+		return request.json()
+
+	def updateIssue(self, repo, issue, title=None, body=None, 
+		assignee=None, state=None, milestone=None, labels=None):
+		url = self.base_url+"repos/"+self._username+"/"+repo+"/issues/"+issue
+		payload = {}
+		if title: payload["title"] = title
+		if body: payload["body"] = body
+		if state: payload["state"] = state
+		if assignee: payload["assignee"] = assignee
+		if milestone: payload["milestone"] = milestone
+		if labels:
+			payload["labels"] = []
+			for label in labels:
+				payload["labels"].append(label)
+		request = requests.post(url, auth=(self._username, self._password),
+			data=payload)
+		return request.json()
+
+	def getIssueComments(self, repo, issue):
+		url = self.base_url+"repos/"+self._username+"/"+repo+"/issues/"+issue+
+		"/comments"
+		request = requests.get(url, auth=(self._username, self._password))
+		comment = request.json()
+		return comments
+
+	def comment(self, repo, issue, body, comment_id=None):
+		url = self.base_url+"repos/"+self._username+"/"+repo+"/issues/"+issue+
+		"/comments"
+		if comment_id:
+			url += "/"+comment_id
+		payload = {}
+		payload["body"] = body
+		request = requests.post(url, auth=(self._username, self._password), 
+			data=payload)
+		comment = request.json()
+		return comment
+
+	def deleteComment(self, repo, issue, comment_id):
+		url = self.base_url+"repos/"+self._username+"/"+repo+"/issues/"+issue+
+		"/comments/"+comment_id
+		request = requests.delete(url, auth=(self._username, self._password))
+		status = request.status
+		return status
+
 class Trello(object):
 	def __init__(self, email, password):
 		self._email = email
