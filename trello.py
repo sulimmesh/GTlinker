bdb1d3e32ac2e68c7312afe2ef09de8a923a8321
Steve <steven.helferich@gmail.com>
2015-09-25 16:58:25 -0700

    finished remaining trello methods

--- trello.py
+++ trello.py
@@ -6,13 +6,43 @@ class Trello(object):
 		self._password = password
 		self.base_url = "https://api.trello.com/1/"
 
+	def getBoards(self):
+		url = self.base_url+"boards/"
+		request = requests.get(url, auth=(self._email, self._password))
+		return request.json()
+
 	def getCards(self, board):
 		url = self.base_url+"boards/"+board+"/cards"
 		request = requests.get(url, auth=(self._email, self._password))
 		return request.json()
 
 	def getCard(self, card):
-		url = self.base_url+"/cards/"+card
+		url = self.base_url+"cards/"+card
 		request = requests.get(url, auth=(self._email, self._password))
 		return request.json()
 
+	def addCard(self, name, desc, idList, idLabels):
+		url = self.base_url+"cards"
+		payload = {}
+		payload["name"] = name
+		payload["desc"] = desc
+		payload["idList"] = idList
+		payload["idLabels"] = idLabels
+		request = requests.post(url, auth=(self._email, self._password),
+			data=payload)
+		return request.json()
+
+	def moveCard(self, card, list_id):
+		url = self.base_url+"cards/"+card+"/idList"
+		payload = {}
+		payload["value"] = list_id
+		request = requests.put(url, auth=(self._email, self._password))
+		return request.json()
+
+	def addComment(self, card, body):
+		url = self.base_url+"cards/"+card+"/actions/comments"
+		payload = {}
+		payload["text"] = body
+		request = requests.post(url, auth=(self._email, self._password))
+		return request.json()
+		
