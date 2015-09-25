9bd4c0476fec724f7cdf63243b6ff71077eddb76
Steve <steven.helferich@gmail.com>
2015-09-25 16:29:21 -0700

    adding trello api methods.

--- trello.py
+++ trello.py
@@ -4,3 +4,15 @@ class Trello(object):
 	def __init__(self, email, password):
 		self._email = email
 		self._password = password
+		self.base_url = "https://api.trello.com/1/"
+
+	def getCards(self, board):
+		url = self.base_url+"boards/"+board+"/cards"
+		request = requests.get(url, auth=(self._email, self._password))
+		return request.json()
+
+	def getCard(self, card):
+		url = self.base_url+"/cards/"+card
+		request = requests.get(url, auth=(self._email, self._password))
+		return request.json()
+
